1c2fe76e436912a32b3fd6f85edcc9a7f0b54cc3
Cody Kelly <cody.kelly@ucdenver.edu>
2022-03-20 12:41:18 -0600

    Changed event to JSON

--- src/App.js
+++ src/App.js
@@ -2,28 +2,7 @@ import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className='center-contents'>
-      <div className='card'>
-        <h1>Welcome</h1>
-        <p style={{ maxWidth: '300px' }}>
-          ...to your new burger stand!
-          <br />
-          <br />
-          As you gaze proudly upon the ramshackle eatery you pieced together in
-          the last five minutes, you realize you don't yet have a name for this
-          fine establishment!
-          <br />
-          <br />
-          Let's give your stand a name:
-        </p>
-        <form>
-          <input type='text' />
-          <input type='submit' className='btn' value="Let's go!" />
-        </form>
-      </div>
-    </div>
-  );
+  return <Fragment></Fragment>;
 }
 
 export default App;
